cf46569685339e4440d42f3da2205d724ef48334
Emon02 <abdulalimemon11@gmail.com>
2022-07-25 04:04:33 +0600

    footer and favicon

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import { Route, Routes } from 'react-router';
 import Header from './Pages/Shared/Header/Header';
 import Home from './Pages/Home/Home/Home';
+import Footer from './Pages/Shared/Footer/Footer';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home></Home>}></Route>
       </Routes>
+      <Footer></Footer>
     </div>
   );
 }
--- src/Pages/Shared/Footer/Footer.js
+++ src/Pages/Shared/Footer/Footer.js
@@ -1,10 +1,14 @@
 import React from 'react';
 
 const Footer = () => {
+    const today = new Date();
+    const year = today.getFullYear();
     return (
-        <div>
-            ...
-        </div>
+        <footer className="bg-dark text-light">
+            <div className="container py-5">
+                <h5 className="text-center py-3">Copyright &copy; Your Doctor {year}. All rights reserved.</h5>
+            </div>
+        </footer>
     );
 };
 
